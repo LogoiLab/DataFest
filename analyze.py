f8e5db6c4fceebefcd113c543f99fc0e93007c76
Zachary Zwierko <zzwierko@gmail.com>
2018-04-14 11:47:46 -0400

    Stuff.

--- analyze.py
+++ analyze.py
@@ -7,6 +7,7 @@ Spring 2018
 
 import sqlite3
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -15,13 +16,10 @@ def random_n(num_rows):
     return pd.read_sql(f"""
 SELECT *
 FROM jobs
-WHERE jobid IN (SELECT jobid
-                FROM jobs
-                ORDER BY RANDOM()
-                LIMIT {num_rows})
+LIMIT {num_rows}
 """, conn)
 
 conn = sqlite3.connect("data/indeed.db")
 
-jobs = random_n(100000)
+jobs = random_n(5000000)
 jobs.replace("", np.nan, inplace=True)
--- zachary.py
+++ zachary.py
@@ -1,7 +1,22 @@
 # Zachary Zwierko, 04/14/2018
 
+from sklearn.preprocessing import MinMaxScaler
+
 from analyze import *
+
+min_max_scaler = MinMaxScaler()
 
 # Better description == better review?
 q2 = jobs[["descriptionWordCount", "numReviews"]]
 q2 = q2.dropna()
+
+q2 = pd.DataFrame(min_max_scaler.fit_transform(q2.values))
+
+sns.regplot("descriptionWordCount", "numReviews", data=jobs, fit_reg=False, scatter_kws={"s": 1})
+plt.show()
+
+# Can you tell how highly rated a company is from clicks?
+# q4 = jobs[["avgOverallRating", "clicks"]]
+# q4 = q4.dropna()
+# sns.regplot("descriptionWordCount", "numReviews", data=jobs)
+# plt.show()
