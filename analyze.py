192d2f3658615a7ad433df7e5fb3c939e887b7b3
Zachary Zwierko <zzwierko@gmail.com>
2018-04-14 09:55:05 -0400

    * Fixed ugly Chad code. * Got rid of empty strings in DataFrame.

--- analyze.py
+++ analyze.py
@@ -7,6 +7,7 @@ Spring 2018
 
 import sqlite3
 
+import numpy as np
 import pandas as pd
 
 with sqlite3.connect("data/indeed.db") as conn:
@@ -22,3 +23,5 @@ LIMIT 100000
     rows = jobs.fetchall()
 
 data = pd.DataFrame(rows, columns=names)
+data.replace('', np.nan, inplace=True)
+data = data.dropna()
