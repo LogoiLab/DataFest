763c6c8764c27f48ded079fc782c598ddc7cfc1b
Zachary Zwierko <zzwierko@gmail.com>
2018-04-15 00:07:09 -0400

    Finished compare function().

--- plotting.py
+++ plotting.py
@@ -1,25 +1,30 @@
 #!/usr/bin/env python3
 # Zachary Zwierko, 04/14/2018
 
+from scipy.stats import pearsonr
+
 from analyze import *
 
 ROW_N = 500000
 
-def compare(x, y, plot=True, title=None, xlabel=None, ylabel=None):
-    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(10, 8))
+def compare(x, y, title=None, xlabel=None, ylabel=None):
+    with plt.xkcd():
+        fig, axes = plt.subplots(2, 2, figsize=(10, 8))
 
-    sample1 = random_n(ROW_N)[[x, y]].dropna()
-    sample2 = random_n(ROW_N)[[x, y]].dropna()
-    sample3 = random_n(ROW_N)[[x, y]].dropna()
-    sample4 = random_n(ROW_N)[[x, y]].dropna()
+        for row in axes:
+            for ax in row:
+                sample = random_n(ROW_N)[[x, y]].dropna()
 
-    sns.regplot(x=x, y=y, data=sample1, ax=ax1)
-    sns.regplot(x=x, y=y, data=sample2, ax=ax2)
-    sns.regplot(x=x, y=y, data=sample3, ax=ax3)
-    sns.regplot(x=x, y=y, data=sample4, ax=ax4)
+                _, pval = pearsonr(sample[x], sample[y])
 
-    plt.xlabel(xlabel or x)
-    plt.ylabel(ylabel or y)
-    plt.show()
+                sns.regplot(x=x, y=y, data=sample,
+                            ax=ax, fit_reg=False, scatter_kws={"s": 1}, marker=".")
+                ax.text(0.5, 0.5, f"p-value: {pval:.3f}", transform=ax.transAxes, fontsize=10)
+
+                plt.title(title)
+                plt.xlabel(xlabel or x)
+                plt.ylabel(ylabel or y)
+
+        plt.show()
 
 compare("estimatedSalary", "clicks")
