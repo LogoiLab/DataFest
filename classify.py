d28697559fa39bab7307b34cad9cd0a584b35215
Chad Baxter <cbaxter@mail.umw.edu>
2018-04-14 21:30:52 -0500

    classifier logic

--- classify.py
+++ classify.py
@@ -2,9 +2,9 @@ import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
 
 from analyze import *
-from plotting import *
 
 distinct_titles = query_db("SELECT DISTINCT normTitle FROM jobs")
 test_vals = query_db(f"SELECT estimatedSalary from jobs limit 500000")
@@ -13,11 +13,13 @@ print(test_vals.std())
 random_sals = np.random.normal(test_vals.mean(), test_vals.std(), 500000)
 
 
-def classify(title, salary, data):
-    matches = data
-    matches = matches[["normTitle" == title]]
-    matches = matches[["estimatedSalary" >= (salary - 50000) and "estimatedSalary" <= (salary + 50000)]]
-    match = matches[["clicks" == matches["clicks"].max()]]
+def classify(title, salary):
+    matches = test_data
+    matches = matches[matches["normTitle"] == title]
+    matches = matches[(matches["estimatedSalary"] >= (salary - 50000)) & (matches["estimatedSalary"] <= (salary + 50000))]
+    match = matches[matches["clicks"] == matches["clicks"].max()]
+    print(match)
+    sys.stdout.flush()
     return match
 
 
@@ -25,8 +27,9 @@ match_list = pd.DataFrame()
 test_data = random_n(1000000)
 
 for sal in random_sals:
-    match_list.append(classify(distinct_titles.sample(n=1), sal, test_data))
+    match_list.append(classify(distinct_titles.sample(n=1), sal))
     print(sal)
+    sys.stdout.flush()
 
 print(match_list.head())
 print(match_list.tail())
