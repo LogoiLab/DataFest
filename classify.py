0c3a1ac1dd9cc1e2d38dd7784df34d6deefef1b8
Zachary Zwierko <zzwierko@gmail.com>
2018-04-14 23:15:50 -0400

    Fixed Chad's shit.

--- classify.py
+++ classify.py
@@ -1,8 +1,9 @@
+#!/usr/bin/env python3
+
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import sys
 
 from analyze import *
 
@@ -16,20 +17,16 @@ random_sals = np.random.normal(test_vals.mean(), test_vals.std(), 500000)
 def classify(title, salary):
     matches = test_data
     matches = matches[matches["normTitle"] == title]
-    matches = matches[(matches["estimatedSalary"] >= (salary - 50000)) & (matches["estimatedSalary"] <= (salary + 50000))]
+    matches = matches[matches["estimatedSalary"].between(salary-5000, salary+5000, inclusive=True)]
     match = matches[matches["clicks"] == matches["clicks"].max()]
-    print(match)
-    sys.stdout.flush()
     return match
 
 
 match_list = pd.DataFrame()
-test_data = random_n(1000000)
+test_data = random_n(500000)
 
 for sal in random_sals:
     match_list.append(classify(distinct_titles.sample(n=1), sal))
-    print(sal)
-    sys.stdout.flush()
 
 print(match_list.head())
 print(match_list.tail())
