b1b6a036d6a291091ae7943f3fffd10461747915
Zachary Zwierko <zzwierko@gmail.com>
2018-04-15 00:48:29 -0400

    Added more compare plot stuff.

--- plotting.py
+++ plotting.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 # Zachary Zwierko, 04/14/2018
 
+import random
+
 from scipy.stats import pearsonr
 
 from analyze import *
 
-ROW_N = 500000
+ROW_N = 50
 
-def compare(x, y, title=None, xlabel=None, ylabel=None):
+def compare(x, y, is_cat=False, title=None, xlabel=None, ylabel=None):
     with plt.xkcd():
         fig, axes = plt.subplots(2, 2, figsize=(10, 8))
 
@@ -15,16 +17,22 @@ def compare(x, y, title=None, xlabel=None, ylabel=None):
             for ax in row:
                 sample = random_n(ROW_N)[[x, y]].dropna()
 
-                _, pval = pearsonr(sample[x], sample[y])
+                if is_cat:
+                    sns.violinplot(x=x, y=y, data=sample,
+                                   ax=ax, orient="vertical")
+                    ax.set_xticks([])
+                else:
+                    _, pval = pearsonr(sample[x], sample[y])
 
-                sns.regplot(x=x, y=y, data=sample,
-                            ax=ax, fit_reg=False, scatter_kws={"s": 1}, marker=".")
-                ax.text(0.5, 0.5, f"p-value: {pval:.3f}", transform=ax.transAxes, fontsize=10)
+                    sns.regplot(x=x, y=y, data=sample,
+                                ax=ax, fit_reg=False, scatter_kws={"s": 1}, marker=".")
+
+                    ax.text(0.5, 0.5, f"p-value: {pval:.4f}", transform=ax.transAxes, fontsize=10)
 
                 plt.title(title)
                 plt.xlabel(xlabel or x)
                 plt.ylabel(ylabel or y)
 
-        plt.show()
+        plt.savefig(f"plot{random.randint(0, 10000)}.png")
 
-compare("estimatedSalary", "clicks")
+compare("normTitleCategory", "clicks", is_cat=True)
