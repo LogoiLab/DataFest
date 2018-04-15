6e4b05a1fe892b0c48fdc8f833f0efb7d1db90b6
David Zamojda <zamojdadavid@gmail.com>
2018-04-15 10:16:46 -0400

    code stuff

--- David.py
+++ David.py
@@ -8,8 +8,31 @@ Created on Sat Apr 14 10:38:59 2018
 
 from analyze import *
 import matplotlib.pyplot as plt
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.preprocessing import MinMaxScaler
+from sklearn import preprocessing
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import ensemble 
 
-states = jobs[["stateProvince", "city", "country", "industry", "normTitle", "experienceRequired", "estimatedSalary"]]
-sns.countplot(data = jobs, x = "country")
+# "normTitle", "normTitleCategory",  "normTitle","educationRequirements", "stateProvince", "city","country", 
+
+
+jobs2 = jobs[["estimatedSalary", "licenseRequiredJob","supervisingJob"]]
+
+data_y = jobs["experienceRequired"]
+
+print(jobs.head())
+#stuff = pd.get_dummies(jobs2, columns=["stateProvince","country", "city"])
+
+#jobs = jobs.drop("jobLanguage")
+#sns.countplot(data = jobs, x = "country")
+
+#sns.regplot(y = "estimatedSalary", x = "experienceRequired", data = jobs, )
+#plt.show()
+
+#data_y = stuff["normTitleCategory"]
+
+sns.regplot(x = "clicks", y= "estimatedSalary", data = jobs)
 
 
