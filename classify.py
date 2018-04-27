6e771017dc82d987134eee7c8ddc100732b6e5e2
Chad Baxter <cbaxter@mail.umw.edu>
2018-04-26 19:34:52 -0500

    classify working

--- classify.py
+++ classify.py
@@ -4,34 +4,35 @@ import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.stats import pearsonr
+import random
 
 from analyze import *
 
-distinct_titles = query_db("SELECT DISTINCT normTitle FROM jobs")
-test_vals = query_db(f"SELECT estimatedSalary from jobs limit 50")
+
+distinct_titles = query_db("SELECT DISTINCT normTitleCategory FROM jobs")
+test_vals = query_db(f"SELECT estimatedSalary from jobs limit 5000")
 print(test_vals.mean())
 print(test_vals.std())
-random_sals = np.random.normal(test_vals.mean(), test_vals.std(), 50)
+random_sals = np.random.normal(test_vals.mean(), test_vals.std(), 5000)
 
 
 def classify(title, salary):
     matches = test_data
-    matches = matches[matches["normTitle"] == title]
+    matches = matches[matches["normTitleCategory"] == title]
     matches = matches[matches["estimatedSalary"].between(salary-5000, salary+5000, inclusive=True)]
     match = matches[matches["clicks"] == matches["clicks"].max()]
     return match
 
 
 match_list = pd.DataFrame()
-test_data = random_n(50)
+test_data = random_n(5000)
 
 for sal in random_sals:
-    match_list.append(classify(distinct_titles.sample(n=1), sal))
+    match_list = match_list.append(classify(str(distinct_titles.sample(n=1).iloc[0][0]), sal))
 
 print(match_list.head())
 print(match_list.tail())
-
-match_list = match_list[['normTitle', 'clicks', 'estimatedSalary']]
 
 with plt.xkcd():
     fig, axes = plt.subplots(2, 2, figsize=(10, 8))
